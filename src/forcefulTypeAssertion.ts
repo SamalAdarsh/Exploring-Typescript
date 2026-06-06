803182a74d931bf73a47bae2b1715ee0bc15ed97
Adarsh <codewithadarsh007@gmail.com>
2026-06-06 20:21:50 +0530

    Type Assertion Forceful

---
+++ src/forcefulTypeAssertion.ts
@@ -0,0 +1,15 @@
+let response: any = '42';
+
+let numericLength:number = (response as string).length;
+
+type Book ={
+name: string;
+}
+
+let bookString = '{"name":"Road not taken"}';
+let bookObject = JSON.parse(bookString) as Book;
+
+
+console.log(bookObject);
+
+
