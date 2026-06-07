c14e3793a370afc59e0d4689cb79f54050185514
Adarsh <codewithadarsh007@gmail.com>
2026-06-07 17:11:22 +0530

    Tuples

---
+++ src/tuple.ts
@@ -0,0 +1,16 @@
+let coffeeTuples: [string,number];
+
+coffeeTuples=["Latte",2];
+// coffeeTuples=[2,"Latte"] Order should be same as data type
+
+let coffeeOrder: [string,number, boolean?];
+
+coffeeOrder=["Latte",2];
+coffeeOrder=["Latte",3,true];
+
+//named tuples
+const coffeeItems: [name: string,price:number] = ["Latte",25];
+
+//read-only tuples
+
+const location: readonly [number,number] = [33.88,99.01];
