8b15120c527191fc8ca637ea5aa0fd5bfec32e74
Adarsh <codewithadarsh007@gmail.com>
2026-06-17 13:32:11 +0530

    Axios

---
+++ src/axios.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import  type {AxiosResponse} from "axios";
+
+interface ToDo{
+    userId: number;
+    id:number;
+    title: string;
+    completed: boolean;
+}
+
+// axios.get('https://example.com/data').then(response=>{
+//     console.log(response.data)
+// })
+
+
+const fetchData = async()=>{
+    try{
+        const response: AxiosResponse<ToDo> = await axios.get(
+            "https://jsonplaceholder.typicode.com/todos/1"
+        )
+
+        console.log("ToDo", response.data);
+    }
+    catch(error:any){
+        if(axios.isAxiosError(error)){
+            console.log("Axios error", error.message);
+
+            if(error.response){
+                console.log(error.response.status);
+            }
+        }
+
+    }
+}
